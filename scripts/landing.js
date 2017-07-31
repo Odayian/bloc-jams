58bb88f4317f6f6a41f28e48a35a6cba6aa85610
Ben Durbin <BWDurbin@gmail.com>
2017-07-31 19:08:52 -0400

    correction for checkpoint 8

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,14 @@
 var animatePoints = function() {
 
     var points = document.getElementsByClassName('point');
-
     var revealPoint = function(point){
-        for(var i = 0; i < point.length; i++){
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateX(0) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateX(0) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateX(0) translateY(0)";
-        }
+            point.style.opacity = 1;
+            point.style.transform = "scaleX(1) translateX(0) translateY(0)";
+            point.style.msTransform = "scaleX(1) translateX(0) translateY(0)";
+            point.style.WebkitTransform = "scaleX(1) translateX(0) translateY(0)";
     }
-    revealPoint(points);
+
+    for(var i = 0; i < points.length; i++){
+        revealPoint(points[i]);
+    }
 };
